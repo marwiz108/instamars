adb99b80f5ff293eb066ceebd52b3a26c3c842c6
Marwa Elali <marwaelali@gmail.com>
2018-07-04 13:50:20 +0100

    editing/updating posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -11,8 +11,9 @@ class PostsController < ApplicationController
     @post = Post.new
   end
 
-  # def edit
-  # end
+  def edit
+    @post = Post.find(params[:id])
+  end
 
   def create
     @post = Post.create(post_params)
@@ -23,8 +24,14 @@ class PostsController < ApplicationController
     end
   end
 
-  # def update
-  # end
+  def update
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      redirect_to posts_path
+    else
+      render 'edit'
+    end
+  end
 
   # def destroy
   # end
